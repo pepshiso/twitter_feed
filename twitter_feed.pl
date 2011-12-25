fd7a2a00445a61519fb51ac069dd693d3f771cd9
pepshiso <pepshiso9@gmail.com>
2011-12-25 16:35:23 +0900

    Replaced XML::LibXML with XML::Simple.

--- twitter_feed.pl
+++ twitter_feed.pl
@@ -2,20 +2,17 @@
 use strict;
 use warnings;
 use autodie;
-use XML::LibXML;
+use XML::Simple;
+use HTTP::Lite;
 
 if (@ARGV != 1) {
     die "usage: perl twitter_feed.pl [username]\n";
 }
+my $username = $ARGV[0];
 
-my $doc = XML::LibXML->load_xml(
-    location => 'http://twitter.com/users/' . $ARGV[0]
-    );
-my $root = $doc->documentElement();
+my $http = HTTP::Lite->new;
+my $req = $http->request("http://twitter.com/users/$username");
+my $id = XML::Simple->new->XMLin($http->body)->{'id'};
 
-my ($id,) = $doc->getElementsByTagName('id');
-$id = $id->textContent;
-print 'tweets: ', 'http://twitter.com/statuses/user_timeline/',
-    $id, ".rss\n";
-print 'favorites: ', 'http://twitter.com/favorites/',
-    $id, ".rss\n";
+print "tweets: http://twitter.com/statuses/user_timeline/$id.rss\n";
+print "favorites: http://twitter.com/favorites/$id.rss\n";
